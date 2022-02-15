ab2467888f35099255725a1c2fee34a0499ff8e1
kBrausew <kurt.brausewetter@t-online.de>
2022-02-15 17:22:51 +0100

    move env 2022-02-15

--- admin/words.js
+++ admin/words.js
@@ -1,41 +1,7 @@
 /*global systemDictionary:true */
-"use strict";
+'use strict';
 
 systemDictionary = {
-    "wiegand-tcpip adapter settings": {
-        "en": "Adapter settings for wiegand-tcpip",
-        "de": "Adaptereinstellungen für wiegand-tcpip",
-        "ru": "Настройки адаптера для wiegand-tcpip",
-        "pt": "Configurações do adaptador para wiegand-tcpip",
-        "nl": "Adapterinstellingen voor wiegand-tcpip",
-        "fr": "Paramètres d'adaptateur pour wiegand-tcpip",
-        "it": "Impostazioni dell'adattatore per wiegand-tcpip",
-        "es": "Ajustes del adaptador para wiegand-tcpip",
-        "pl": "Ustawienia adaptera dla wiegand-tcpip",
-        "zh-cn": "wiegand-tcpip的适配器设置"
-    },
-    "option1": {
-        "en": "option1",
-        "de": "Option 1",
-        "ru": "Опция 1",
-        "pt": "Opção 1",
-        "nl": "Optie 1",
-        "fr": "Option 1",
-        "it": "opzione 1",
-        "es": "Opción 1",
-        "pl": "opcja 1",
-        "zh-cn": "选项1"
-    },
-    "option2": {
-        "en": "option2",
-        "de": "Option 2",
-        "ru": "вариант 2",
-        "pt": "opção 2",
-        "nl": "Optie 2",
-        "fr": "Option 2",
-        "it": "opzione 2",
-        "es": "opcion 2",
-        "pl": "Opcja 2",
-        "zh-cn": "选项2"
-    }
+    "setup_info": {                                  "en_US": "Door Accesss Controller",                         "en": "Door Accesss Controller"},
+    "wiegand_setup": {                               "en_US": "Setup",                                           "en": "Setup"},
 };
