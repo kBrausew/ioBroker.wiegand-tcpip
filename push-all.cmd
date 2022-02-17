7706ab1b0148f09163d0ec6b61681e3f8e85fb74
kBrausew <kurt.brausewetter@t-online.de>
2022-02-17 08:33:35 +0100

    BE1GV046 A300875 17.02.2022  8:33:35,05

--- push-all.cmd
+++ push-all.cmd
@@ -2,14 +2,22 @@
 cd /d %~dp0
 
 :BEGIN
+	echo.# ADD
 	git add .
 	if errorlevel 1 goto :ERROR
+	echo.#
+	echo.#
 	
+	echo.# COMMIT
 	git commit -m "%COMPUTERNAME% %USERNAME% %DATE% %TIME%"
 	if errorlevel 1 goto :ERROR
+	echo.#
+	echo.#
 	
+	echo.# PUSH
 	git push rep master
 	if errorlevel 1 goto :ERROR
+	echo.#
 	
 	echo.# Finish
 	pause
