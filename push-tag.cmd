b13ab583341a1f6a57339ff8b27c766d34908319
kBrausew <kurt.brausewetter@t-online.de>
2022-03-09 09:54:37 +0100

    CMD-Scripts: BE1GV046 A300875 09.03.2022  9:54:37,20

---
+++ push-tag.cmd
@@ -0,0 +1,30 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:BEGIN
+    if .%1==.     goto :ERROR_PARAM1
+    rem if .%2==.     goto :ERROR_PARAM2
+    if not .%3==. goto :ERROR_PARAM3
+
+
+    git tag -a "%~1" -m "Version: %~1 =>  %~2"
+    echo git push rep --tags
+    goto :FINISH
+
+
+:ERROR_PARAM1
+	echo.# Geben sie eine Version an die sie erstellen wollen
+	pause
+	goto :FINISH
+
+:ERROR_PARAM2
+	echo.# Kommentare sind optional
+	pause
+	goto :FINISH
+
+:ERROR_PARAM3
+	echo.# Stellen sie Kommentare bitte in doppelte Hochkommata
+	pause
+	goto :FINISH
+
+:FINISH
